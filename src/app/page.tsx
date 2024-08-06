50dce6b70a1d099cd2da2b9a12de8b121213367d
“Brenno <dev.brennoeudes@gmail.com>
2024-08-06 20:38:04 -0300

    tw extensions example

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="p-4 bg-slate-400 text-slate-200 h-screen"> {/* consegue aplicar nas classes as extensões definidas em twconfig */}
+      <h1 className="font-bold text-5xl text-brenno">Hello, Tailwind!</h1>
+      <h2 className="">Hello, Tailwind!</h2>
+      <h2 className="">Hello, Tailwind!</h2>
+    </div>
+  );
+}
