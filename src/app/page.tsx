9befd911abf1b9eb5f6a3b3c9132459528c49b31
“Brenno <dev.brennoeudes@gmail.com>
2024-08-07 21:05:49 -0300

    eslint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ export default function Home() {
   );
 }
 
-// Anotações
-/* O TW por padrão parte das especificações para mobile! (mobile first) */
-/* Evite usar valores arbitrários fora do padrão do TW (ex: max-w-[700px]. Crie o atalho em extends no tailwind/config e aplique no código (ex: colors brenno) */
+// Anotações:
+// O TW por padrão parte das especificações para mobile! (mobile first)
+// Evite usar valores arbitrários fora do padrão do TW (ex: max-w-[700px]. Crie o atalho em extends no tailwind/config e aplique no código (ex: colors brenno)
+// TW possui uma ordem semântica: classes estruturais, classes de estilo (cores, fontes) e depois classes de seletores (ex: hover etc). Não esqueça de >reload p/ resetar o VSCode no final.
+
