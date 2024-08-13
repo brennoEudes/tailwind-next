0199be91f022de1e975edc2583f75a218ad3c3c1
“Brenno <dev.brennoeudes@gmail.com>
2024-08-13 20:58:36 -0300

    logo and searcj input structure

---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,20 @@
+import { Logo } from './Logo'
+import { Search } from 'lucide-react'
+
+export function Sidebar() {
+  return (
+    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+      {/* TW (space-y) define espaçamento interno dos elementos filhos da div principal */}
+      <Logo />
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        {/* TW tb vem com sombras padrões! (shadow) */}
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          // flex-1: 1 1 0%; em q 1 cresce o máx do espaço disponível, 1 encolhe o máx p/ se adaptar quando houve outro elemnto e 0% largura mín
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+          placeholder="Search"
+        />
+      </div>
+    </aside>
+  )
+}
