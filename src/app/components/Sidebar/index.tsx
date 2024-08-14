98ffb1643eec0f2ca8ca4d8dac84259d33814261
“Brenno <dev.brennoeudes@gmail.com>
2024-08-13 21:53:35 -0300

    widget, and usedSpace, MainNavigation deleted

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,10 +1,21 @@
+import {
+  Home,
+  BarChart,
+  SquareStack,
+  CheckSquare,
+  Flag,
+  User,
+  Search,
+  LifeBuoy,
+  Cog,
+} from 'lucide-react'
 import { Logo } from './Logo'
-import { Search } from 'lucide-react'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       {/* TW (space-y) define espaçamento interno dos elementos filhos da div principal */}
       <Logo />
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -16,7 +27,22 @@ export function Sidebar() {
           placeholder="Search"
         />
       </div>
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        {/* Passado os nomes dos componentes como referência em icon, a fim de evitar a repetição do cód e aproveitar NavItem */}
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={User} />
+      </nav>
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
