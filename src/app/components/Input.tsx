e869da08ce68fc9d6b60ef55ecda86dbc18250f0
“Brenno <dev.brennoeudes@gmail.com>
2024-08-19 22:36:11 -0300

    input component pattern

---
+++ src/app/components/Input.tsx
@@ -0,0 +1,35 @@
+import { ComponentProps } from 'react'
+
+// PATTERN DE COMPOSIÇÃO DE COMPONENTES
+// É quando dividmos um componente em partes menores, cada uma com uma responsabilidade específica, 
+// a fim de trazer uma maior acessibilidade e reutilização de código.
+
+type InputPrefixProps = ComponentProps<'div'>
+
+export function InputPrefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+type InputControlProps = ComponentProps<'input'>
+
+export function InputControl(props: InputControlProps) {
+  return (
+    <input
+      // flex-1: 1 1 0%; em q 1 cresce o máx do espaço disponível, 1 encolhe o máx p/ se adaptar quando houve outro elemnto e 0% largura mín
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      placeholder="Search"
+      {...props}
+    />
+  )
+}
+
+export type InputRootProps = ComponentProps<'div'>
+
+export function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm" // TW tb vem com sombras padrões! (shadow)
+      {...props}
+    />
+  )
+}
--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -13,21 +13,20 @@ import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import { InputControl, InputPrefix, InputRoot } from '../input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       {/* TW (space-y) define espaçamento interno dos elementos filhos da div principal */}
       <Logo />
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        {/* TW tb vem com sombras padrões! (shadow) */}
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          // flex-1: 1 1 0%; em q 1 cresce o máx do espaço disponível, 1 encolhe o máx p/ se adaptar quando houve outro elemnto e 0% largura mín
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl />
+      </InputRoot>
+
       <nav className="space-y-0.5">
         {/* Passado os nomes dos componentes como referência em icon, a fim de evitar a repetição do cód e aproveitar NavItem */}
         <NavItem title="Home" icon={Home} />
