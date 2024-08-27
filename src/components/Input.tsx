6047d3963cb2ee16d3492f3bc15c6d5d3f1c432e
“Brenno <dev.brennoeudes@gmail.com>
2024-08-26 22:08:05 -0300

    focus effect

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -16,7 +16,7 @@ export function InputControl(props: InputControlProps) {
   return (
     <input
       // flex-1: 1 1 0%; em q 1 cresce o máx do espaço disponível, 1 encolhe o máx p/ se adaptar quando houve outro elemnto e 0% largura mín
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none" // TW: outline reseta borda default!
       placeholder="Search"
       {...props}
     />
@@ -27,8 +27,9 @@ export type InputRootProps = ComponentProps<'div'>
 
 export function InputRoot(props: InputRootProps) {
   return (
+    // Div não possui foco, então não precisamos do focus-within p/ aplicar estilo de foco no elemento pai (externo), no caso, o input acima.
     <div
-      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm" // TW tb vem com sombras padrões! (shadow)
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100" // TW tb vem com sombras padrões! (shadow). TW: ring add shadow na borda
       {...props}
     />
   )
--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -13,10 +13,15 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
+      className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700"
       /* TW + Radix: prop data-state (e outras) podem ser custmomizadas. Aqui, mudamos a cor quando o state for active. */
     >
-      <span className="">{title}</span>
+      <span className="rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {/* TW: ring-offset cria um espaço entre o elemento e o anel; 
+        Focus = estilo quando o usuário clicar, já o visible = quando a navegacão for pelo teclado */}
+        {title}
+      </span>
+      {/* TW: group permite ao elemento interno receber o foco quando o pai tb o receber */}
       {isSelected && (
         <motion.div
           layoutId="activeTab"
