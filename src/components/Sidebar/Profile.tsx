6bb4312121949b6a246a41b85d5c1695f9a47ba6
“Brenno <dev.brennoeudes@gmail.com>
2024-09-02 23:14:47 -0300

    img adjust, sidebar scroll adjust

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,21 +1,25 @@
 import { LogOut } from 'lucide-react'
 import { Button } from '../Button'
 
+export interface ProfileProps {}
+
 export function Profile() {
   return (
-    <div className="grid grid-cols-profile items-center gap-3">
+    <div className="flex items-center gap-3">
       <img
         src="https://github.com/brennoEudes.png"
-        className="h-4 w-4 rounded-full"
+        className="h-10 w-10 rounded-full"
         alt="profile picture"
       />
-      <div className="flex flex-col truncate">
+      <div className="flex flex-col">
         <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Brenno Eudes
         </span>
-        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">brenno@gmail.com</span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          brenno@gmail.com
+        </span>
       </div>
-      <Button type="button" variant="ghost">
+      <Button type="button" variant="ghost" className="ml-auto">
         <LogOut className="h-5 w-5 text-zinc-500" />
       </Button>
     </div>
